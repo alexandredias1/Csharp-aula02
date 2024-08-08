dd4d7b45b69b9b0595e5262569ee0bb451623c7a
62127702024.2 <62127702024.2@62DEDS0J504>
2024-08-07 21:56:12 -0300

    Adicionar arquivos de projeto.

---
+++ projetoteste/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Digite um número: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int soma = 0;
+
+if (N > 0)
+{
+    for (int i = 1; i <= N; i++)
+    {
+        soma += i;
+    }
+}
+else
+{
+    for (int i = N; i <= 1; i++)
+    {
+        soma += i;
+    }
+
+}
+Console.WriteLine($"A soma de 1 a {N} é: {soma}");
